6679ad6169d0dd48ae01aa187d4dff098dd1c75b
Yasaman Mirmohammad <y.mirmohammad@yahoo.com>
2017-11-17 01:19:37 -0800

    Add files via upload

---
+++ MiniProject/Windows/SingleThread/cmake-build-debug/CMakeFiles/SingleThread.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for SingleThread.
+# This may be replaced when dependencies are built.
---
+++ MiniProject/Windows/SingleThread/main.c
@@ -0,0 +1,40 @@
+/*************************************************
+ * C program to count no of lines, words in a file.			 *
+ *************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    FILE *file;
+    char filename[100];
+    char ch;
+    int  wordcount, charcount;
+
+    // Initialize counter variables
+    wordcount = 0;
+    charcount = 0;
+
+    // Prompt user to enter filename
+    printf("Enter a filename :");
+    gets(filename);
+
+    // Open file in read-only mode
+    file = fopen(filename,"r");
+
+    // If file opened successfully, then write the string to file
+    if ( file )
+    {
+        //Repeat until End Of File character is reached.
+        while ((ch=getc(file)) != EOF) {
+            // Increment character count if NOT new line or space
+            if (ch != ' ' && ch != '\n') { ++charcount; }
+
+            // Increment word count if new line or space character
+            if (ch == ' ' || ch == '\n') { ++wordcount; }
+
+
+
+
+
+        }
