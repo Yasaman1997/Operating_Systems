fa96468ba04ce538a4fd45dc7cd6f603d8780c2b
Yasaman Mirmohammad <y.mirmohammad@yahoo.com>
2017-11-17 01:41:24 -0800

    Add files via upload

---
+++ MiniProject/Windows/MultiProcess/cmake-build-debug/CMakeFiles/MultiProcess.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MultiProcess.dir/main.c.o"
+  "MultiProcess.pdb"
+  "MultiProcess"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MultiProcess.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
