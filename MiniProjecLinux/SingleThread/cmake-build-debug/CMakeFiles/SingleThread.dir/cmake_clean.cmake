f124f92577be29c5ebc2961bfdc3e81005bc05f2
Yasaman Mirmohammad <y.mirmohammad@yahoo.com>
2017-11-17 00:42:37 -0800

    Add files via upload

---
+++ MiniProjecLinux/SingleThread/cmake-build-debug/CMakeFiles/SingleThread.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SingleThread.dir/main.c.o"
+  "SingleThread.pdb"
+  "SingleThread"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SingleThread.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
