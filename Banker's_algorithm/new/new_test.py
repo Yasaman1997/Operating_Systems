a6714d25e5a185f68a3805cfbab1875f5684a7f2
Yasaman Mirmohammad <y.mirmohammad@yahoo.com>
2017-12-15 09:13:37 -0800

    A simple solution for preventing deadlock

---
+++ Banker's_algorithm/new/new_test.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+n_processes = int(input('Number of processes? '))
+n_resources = int(input('Number of resources? '))
+
+#available sources
+available_resources = [int(x) for x in input('Claim vector? ').split(' ')]
+
+#current allocated
+currently_allocated =  \
+    np.array(
+    [[int(x) for x in input('Currently allocated for process ' + str(i + 1) + '? ').split(' ')] for i in
+     range(n_processes)])
+
+#max demand
+max_demand = np.array([[int(x) for x in input('Maximum demand from process ' + str(i + 1) + '? ').split(' ')] for i in
+                       range(n_processes)])
+
+#total available to be allocated
+total_available = available_resources - np.sum(currently_allocated, axis=0)
+
+
+# An array with n_processes 1's to indicate if process is yet to run
+running = np.ones(n_processes)
+
+
+while np.count_nonzero(running) > 0:
+    at_least_one_allocated = False
+    for p in range(n_processes):
+        if running[p]:
+            if all(i >= 0 for i in total_available - (max_demand[p] - currently_allocated[p])):
+                at_least_one_allocated = True
+                print(str(p) + ' is running')
+                running[p] = 0
+                total_available += currently_allocated[p]
+    if not at_least_one_allocated:
+        print('Unsafe')
+        exit()
+
+print('Safe');
